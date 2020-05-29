9fa61131c84de2427162f2364c8937f31a490bf1
wuxx <wujianhua0924@gmail.com>
2020-05-29 19:22:01 +0800

    commit v2.3b (support drag-and-drop (v2.3 has some bug which don't support drag-and-drop))

---
+++ software/v2.3b/projectfiles/uvision/stm32f103xb_stm32f103rb_if/RTE/_stm32f103xb_stm32f103rb_if/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f103xb_stm32f103rb_if' 
+ * Target:  'stm32f103xb_stm32f103rb_if' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
